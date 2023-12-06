7fe0540a374680df90b48dd863010c2a6e87f8a1
fazrianbaryf <fazrianbaryf@gmail.com>
2023-12-07 00:50:10 +0700

    Update Crud Data Guru

--- admin/backend/prosses-data-guru.php
+++ admin/backend/prosses-data-guru.php
@@ -5,21 +5,23 @@ require '../config/db.php';
 if(isset($_POST['add-guru'])){
     global $mysqli;
 
-    $NIP = mysqli_real_escape_string($mysqli, $_POST['NIP']);
     $K_GURU = mysqli_real_escape_string($mysqli, $_POST['kodeGuru']);
+    $NIP = mysqli_real_escape_string($mysqli, $_POST['NIP']);
     $NAME = mysqli_real_escape_string($mysqli, $_POST['namaGuru']);
     $NO_TELP = mysqli_real_escape_string($mysqli, $_POST['no_telp']);
     $P_MAPEL = mysqli_real_escape_string($mysqli, $_POST['pengampuMapel']);
     $JK = mysqli_real_escape_string($mysqli, $_POST['jk']);
 
-    $QueryAddGuru = "INSERT INTO tbl_m_guru(kode_guru, nip_guru, nama_guru, no_telp_guru, jenis_kelamin, pengampu_mapel, created_by_tmg) VALUES ('$NIP','$K_GURU','$NAME','$NO_TELP','$JK','$P_MAPEL', 1)";
+    $QueryAddGuru = "INSERT INTO tbl_m_guru(kode_guru, nip_guru, nama_guru, no_telp_guru, pengampu_mapel, jenis_kelamin, create_date_tmg, create_by_tmg) 
+    VALUES ('".$K_GURU."','".$NIP."','".$NAME."','".$NO_TELP."', '".$P_MAPEL."', '".$JK."', NOW(), 1)";
 
-    $ResultQueryAddGuru = mysqli_query($mysqli, $QueryAddGuru); 
+    
+    $ResultQueryAddGuru = mysqli_query($mysqli, $QueryAddGuru);
 
-    if ($ResultQueryAddGuru){
-        echo "Data Guru Berhasil Di tambahkan"; 
+    if ($ResultQueryAddGuru) {
+        echo "Tambah user berhasil.";
     } else {
-        echo "Gagal menambahkan User" . mysqli_error($mysqli);
+        echo "Gagal menambah user: " . mysqli_error($mysqli);
     }
 
 }
